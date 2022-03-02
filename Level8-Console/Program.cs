b8db0a76eae51ded3533975de0798ae37655f6ac
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-03-02 16:18:34 -0700

    Level 8

---
+++ Level8-Console/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Title = "Defense of Consolas";
+
+Console.Write("Target Row? ");
+int targetRow = Int32.Parse(Console.ReadLine());
+
+Console.Write("Target Column? ");
+int targetColumn = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Deploy to:");
+Console.WriteLine($"({targetRow}, {targetColumn - 1})");
+Console.WriteLine($"({targetRow - 1}, {targetColumn})");
+Console.WriteLine($"({targetRow}, {targetColumn + 1})");
+Console.WriteLine($"({targetRow + 1}, {targetColumn})");
+
+Console.Beep();
