dec47298cb7b8398735810d55fc209b615ea16d2
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-03-18 16:02:03 -0600

    Level 12

---
+++ Level12-Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("The Replicator of D'To");
+int[] five = new int[5];
+
+Console.WriteLine("Please enter five comma delimited numbers. ");
+string? userNumbers = Console.ReadLine() ?? null;
+
+if (!String.IsNullOrWhiteSpace(userNumbers)) {
+  int[] userNumsArr = userNumbers.Split(',').Select(int.Parse).ToArray();
+
+  for (int i = 0; i < five.Length; i++) {
+    five[i] = userNumsArr[i];
+
+    Console.WriteLine($"User Input: {userNumsArr[i]}, Copy: {five[i]}");
+  }
+}
+
+Console.WriteLine("----------------------------");
+Console.WriteLine("The Laws of Freach");
+int[] allValues = new int[] { 22, 33, 10, 700, 1 };
+int minValue = int.MaxValue;
+int sum = 0;
+
+foreach(int val in allValues) {
+  if (val < minValue) minValue = val;
+  sum += val;
+}
+
+float averageValue = (float)sum / allValues.Length;
+
+Console.WriteLine($"The lowest value in the array is {minValue}");
+Console.WriteLine($"The average value in the array is {averageValue}");
