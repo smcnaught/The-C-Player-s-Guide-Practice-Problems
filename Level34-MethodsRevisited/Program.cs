c09f690502e7520bc4fdb50982460f60929a521f
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-09-19 11:17:18 -0600

    Level 34

---
+++ Level34-MethodsRevisited/Program.cs
@@ -0,0 +1,17 @@
+﻿// SAFER NUMBER CRUNCHING
+bool userEnteredNumber = false;
+string getUserNumber () {
+  Console.WriteLine("Enter a number");
+  string input = Console.ReadLine();
+  return input;
+}
+
+while (!userEnteredNumber) {
+  string input = getUserNumber();
+  if (int.TryParse(input, out int value)) {
+    userEnteredNumber = true;
+    Console.WriteLine($"You entered {value}");
+  }
+  else getUserNumber();
+}
+// SAFER NUMBER CRUNCHING
---
+++ Level34-MethodsRevisited/RandomExtensions.cs
@@ -0,0 +1,15 @@
+public static class RandomExtensions {
+  public static double NextDouble(this Random random, double max) {
+    return randon.NextDouble() * max;
+  }
+
+  public static string NextString(this Random random, params string[] values) {
+    int rand = random.Next(0, values.Length);
+    return values[rand];
+  }
+
+  public static bool CoinFlip(this Random random, double percentChance = 50) {
+    int rand = random.Next(1, 101);
+    return rand < percentChance;
+  }
+}
