98c0892db088bc98f1c6396423cda59956aefbcd
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-10-28 10:37:25 -0600

    Level 38 & 39

---
+++ Level38-LambdaExpressions/Program.cs
@@ -0,0 +1,15 @@
+﻿while (true) {
+  Console.Write("Do you want to check if even(1), positive(2) or if your number is a multiple of ten(3)?");
+  int filterChoice = Int32.Parse(Console.ReadLine() ?? "0");
+  Sieve sieve = filterChoice switch {
+    1 => new Sieve(x => x % 2 == 0),
+    2 => new Sieve(x => x >= 0),
+    3 => new Sieve(x => x % 10 == 0)
+  };
+
+  Console.Write("Which number would you like to check?");
+  int playerNumber = Int32.Parse(Console.ReadLine() ?? "0");
+
+  string goodOrBad = sieve.IsGood(playerNumber) ? "good" : "bad";
+  Console.WriteLine($"Your number is {goodOrBad}");
+}
---
+++ Level39-Files/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter your name");
+string playerName = Console.ReadLine();
+
+int score = 0;
+bool escapePressed = false;
+
+bool userHasSaveFile = File.Exists(playerName + ".txt");
+if (userHasSaveFile) {
+  score = Int32.Parse(File.ReadAllText(playerName+".txt"));
+}
+
+while (!escapePressed) {
+  Console.WriteLine("Hit any key. Hit escape to quit.");
+  ConsoleKeyInfo keyInput = Console.ReadKey();
+  if (keyInput.Key != ConsoleKey.Escape) {
+    score++;
+    Console.WriteLine($"You entered {keyInput.Key} - plus 1 to your score!");
+  }
+  else {
+    escapePressed = true;
+    File.WriteAllText(playerName+".txt", score.ToString());
+    Console.WriteLine($"Your score is: {score}");
+  }
+}
