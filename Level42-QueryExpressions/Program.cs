8aed6ba8ce6a3cb75bc6fbbf5ae09316ac2a3dbc
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-11-09 16:14:44 -0700

    Level 42

---
+++ Level42-QueryExpressions/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> lennikNumbers = new List<int>() { 1, 9, 2, 8, 3, 7, 4, 6, 5 };
+IEnumerable<int> filteredNums = from num in lennikNumbers
+                                where num % 2 == 0
+                                orderby num
+                                select num * 2;
+
+List<int> listy = filteredNums.ToList();
+
+foreach (int n in listy) Console.WriteLine(n);
