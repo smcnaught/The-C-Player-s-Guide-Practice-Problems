81856d422b40cfe7eb65304cb89f564b2a3bff47
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-11-09 16:57:51 -0700

    Level 43

---
+++ Level43-Threads/Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+RecentNumbers recentNums = new RecentNumbers { recNum1 = 0, recNum2 = 0 };
+Thread threadOne = new Thread(Generate);
+threadOne.Start(recentNums);
+
+void Generate(object recent) {
+  RecentNumbers recents = (RecentNumbers)recent;
+  while (true) {
+    int latestRandom = random.Next(0, 10);
+    lock(recents) {
+      recents.recNum2 = recents.recNum1;
+      recents.recNum1 = latestRandom;
+    }
+
+    Console.WriteLine(latestRandom);
+    Thread.Sleep(1000);
+  }
+}
+
+while (true) {
+  Console.ReadKey(false);
+  bool isDup;
+  lock(recentNums) {
+    isDup = recentNums.recNum1 == recentNums.recNum2;
+  }
+
+  if (isDup) Console.WriteLine("You found a dup");
+  else Console.WriteLine("No dup found");
+}
---
+++ Level43-Threads/RecentNumbers.cs
@@ -0,0 +1,4 @@
+public class RecentNumbers {
+  public int recNum1;
+  public int recNum2;
+}
