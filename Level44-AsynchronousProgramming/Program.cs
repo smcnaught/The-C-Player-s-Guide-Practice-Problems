178f820b94a9989b0ab47484e43ec51773677e16
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-12-08 12:06:51 -0700

    Level 44 - Asynchronous Programming

---
+++ Level44-AsynchronousProgramming/Program.cs
@@ -0,0 +1,24 @@
+﻿int RandomlyRecreate(string word) {
+  Random random = new Random();
+  bool found = false;
+  int attempts = 0;
+  while(!found) {
+    string newWord = "";
+    for (int i = 0; i < word.Length; i++) newWord += (char)('a' + random.Next(26));
+    found = word == newWord;
+    attempts++;
+  }
+
+  return attempts;
+}
+
+Task<int> RandomlyRecreateAsync(string word) {
+  return Task.Run(() => RandomlyRecreate(word));
+}
+
+Console.WriteLine("Please enter a short word (2-5 letters)");
+string userWord = Console.ReadLine();
+DateTime start = DateTime.Now;
+await RandomlyRecreateAsync(userWord);
+DateTime finish = DateTime.Now;
+Console.WriteLine($"Time elapsed {finish - start}");
